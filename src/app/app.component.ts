59ffd96aa8c1850966da776256bb431699302a31
johnochoa <jjmesa@stefanini.com>
2025-09-05 14:21:23 -0500

    feat: implement convenios management interface with search and table display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
 
+interface Convenio {
+  idConvenio: string;
+  nombreConvenio: string;
+  compania: string;
+  idCliente: string;
+  formaPago: string;
+  idCliente2: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'suramaquetacion';
+  searchForm = {
+    idConvenio: '12345678',
+    nombreConvenio: 'Nombre por default precargado',
+    estadoConvenio: '',
+    compania: '',
+    idCliente: '12345678'
+  };
+
+  estadoOptions = [
+    { label: 'Activo', value: 'activo' },
+    { label: 'Inactivo', value: 'inactivo' },
+    { label: 'Pendiente', value: 'pendiente' }
+  ];
+
+  companiaOptions = [
+    { label: 'Sura', value: 'sura' },
+    { label: 'Bancolombia', value: 'bancolombia' },
+    { label: 'Davivienda', value: 'davivienda' }
+  ];
+
+  convenios: Convenio[] = [
+    {
+      idConvenio: 'Lorem ipsum',
+      nombreConvenio: 'Lorem ipsum',
+      compania: 'Lorem ipsum',
+      idCliente: 'Lorem ipsum',
+      formaPago: 'Lorem ipsum',
+      idCliente2: 'Lorem ipsum'
+    },
+    {
+      idConvenio: 'Lorem ipsum',
+      nombreConvenio: 'Lorem ipsum',
+      compania: 'Lorem ipsum',
+      idCliente: 'Lorem ipsum',
+      formaPago: 'Lorem ipsum',
+      idCliente2: 'Lorem ipsum'
+    }
+  ];
+
+  showMenu(event: Event, overlayPanel: any) {
+    overlayPanel.toggle(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {OverlayPanelModule} from "primeng/overlaypanel";
+import {TableModule} from "primeng/table";
+import {FormsModule} from "@angular/forms";
+import {DropdownModule} from "primeng/dropdown";
+import {ChipsModule} from "primeng/chips";
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OverlayPanelModule,
+    TableModule,
+    FormsModule,
+    DropdownModule,
+    ChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
